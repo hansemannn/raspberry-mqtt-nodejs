5059fb2f518afb4f13769f62866704af660b3b9e
Hans Knöchel <hansemannn@users.noreply.github.com>
2017-04-24 09:36:54 +0200

    Update index.js

--- node-hue-mqtt/index.js
+++ node-hue-mqtt/index.js
@@ -3,13 +3,13 @@
 const mqtt = require('mqtt');
 const hue = require("node-hue-api");
 const chalk = require('chalk');
+const topics = require('./topics')
+const activeTopics = generateActiveTopics();
 
 /**
  * Configure MQTT
  */
 const client = mqtt.connect('mqtt://localhost');
-const allowedTopics = ['lights', 'config'];
-const allowedSubTopics = ['toggle', 'color', 'adduser'];
 
 /**
  * Configure Hue API
@@ -23,138 +23,130 @@ var state = lightState.create();
 const api = new HueApi(host, username);
 const HUE_LOG = chalk.white.bgMagenta('[HUE]')
 
-client.on('connect', function() {
-  console.log('Client connected! Subscribing to ' + chalk.green('/lights/#') + ' ...');
-  client.subscribe('lights/#');
+client.on('connect', function() {  
+  console.log('\nClient connected! Subscribing to ' + chalk.green(activeTopics.join(', ')) + ' ...');
+  client.subscribe(activeTopics);
 
   // Uncomment to publish a color-message to the lights/color topic
   // client.publish('lights/color', '{"deviceID": "1", "color": [255, 0, 255], "state": "on"}');
 
   // Uncomment to publish a toggle-message to the lights/toggle topic
   // client.publish('lights/toggle', '{"on": true}');
-})
+});
 
 client.on('message', function(topic, message) {
-  console.log('New Message:');
+  console.log(chalk.gray('\nNew Message:'));
   console.log('-> ' + chalk.yellow(message.toString()) + '\n');
   handleMessage(topic, message);
 
   // Uncomment to end the client-session after receiving the first valid message
   // client.end();
-})
+});
 
 function handleMessage(topic, message) {
   var topicPaths = topic.split('/')
 
   switch (topicPaths[0]) {
     // lights/#
-    case 'lights':
-      {
-        if (topicPaths.length < 2 || allowedSubTopics.indexOf(topicPaths[1]) === -1) {
-          return console.log('Trying to handle /lights without valid sub-topic. ');
-        }
-        var subTopic = topicPaths[1];
-
-        switch (subTopic) {
-          // lights/toggle
-          case 'toggle':
-            {
-              console.log('Handling ' + chalk.green('lights/toggle') + ' ...')
-
-              try {
-                const parsedMessage = JSON.parse(message);
-
-                if (parsedMessage.on == null || typeof parsedMessage.on !== 'boolean') {
-                  return console.log('Invalid toggle format, needs to be {"on": true|false}.');
-                }
-
-                if (parsedMessage.on === true) {
-                  console.log(HUE_LOG + ' Light state change to ' + chalk.green('{ON}'));
-                  state = state.on();
-                } else {
-                  console.log(HUE_LOG + ' Light state change to ' + chalk.green('{OFF}'));
-                  state = state.off();
-                }
-              } catch (e) {
-                return console.log('Cannot parse message to valid JSON: ' + message);
-              }
-              break;
-            }
-
-            // lights/color
-          case 'color':
-            {
-              console.log('Handling ' + chalk.green('lights/color') + ' ...');
-
-              try {
-                const parsedMessage = JSON.parse(message);
-
-                if (parsedMessage.color !== null && Array.isArray(parsedMessage.color) && parsedMessage.color.length === 3) {
-                  const r = parsedMessage.color[0],
-                        g = parsedMessage.color[1],
-                        b = parsedMessage.color[2];
-
-                  state = state.on().rgb(r, g, b).shortAlert();
-                  console.log(HUE_LOG + ' Color set to ' + chalk.blue('{' + parsedMessage.color.join(',') + '}'));
-                } else {
-                  return console.log('Invalid color format, needs to be {color: [r, g, b]}.');
-                }
-              } catch (e) {
-                return console.log('Cannot parse message to valid JSON: ' + message);
-              }
-
-              break;
-            }
-          default:
-            {
-              return console.log('Cannot handle sub-topic "' + subTopic + '"');
-            }
-        }
-        break;
+    case 'lights': {
+      if (topicPaths.length < 2 || !topics.lights.topics[topicPaths[1]]) {
+        return console.log('Trying to handle /lights without valid sub-topic. ');
       }
-    case 'config':
-      {
-        if (topicPaths.length < 2 || allowedSubTopics.indexOf(topicPaths[1]) === -1) {
-          return console.log('Trying to handle /lights without valid sub-topic. ');
+      var subTopic = topicPaths[1];
+
+      switch (subTopic) {
+        // lights/toggle
+        case 'toggle': {
+          console.log('Handling ' + chalk.green('lights/toggle') + ' ...')
+
+          try {
+            const parsedMessage = JSON.parse(message);
+
+            if (parsedMessage.on == null || typeof parsedMessage.on !== 'boolean') {
+              return console.log('Invalid toggle format, needs to be {"on": true|false}.');
+            }
+
+            if (parsedMessage.on === true) {
+              console.log(HUE_LOG + ' Light state changed to ' + chalk.green('{ON}'));
+              state = state.on();
+            } else {
+              console.log(HUE_LOG + ' Light state changed to ' + chalk.green('{OFF}'));
+              state = state.off();
+            }
+          } catch (e) {
+            return console.log('Cannot parse message to valid JSON: ' + message);
+          }
+          break;
         }
-        var subTopic = topicPaths[1];
 
-        switch (subTopic) {
-          case 'adduser':
-            {
-              function displayUserResult(user) {
-                console.log('[HUE] Created new user:');
-                console.log('[HUE] ' + JSON.stringify(user, null, 2));
+        // lights/color
+        case 'color': {
+          console.log('Handling ' + chalk.green('lights/color') + ' ...');
 
-                username = user.username
-              }
+          try {
+            const parsedMessage = JSON.parse(message);
 
-              function displayError(error) {
-                console.log('[HUE] Error creating new user:');
-                console.log('[HUE] ' + error);
-              }
+            if (parsedMessage.color !== null && Array.isArray(parsedMessage.color) && parsedMessage.color.length === 3) {
+              const r = parsedMessage.color[0],
+                    g = parsedMessage.color[1],
+                    b = parsedMessage.color[2];
 
-              if (nodeUserExists()) {
-                return console.log('[HUE] User "Node API" already exists');
-              }
-
-              HueApi.registerUser(host, name)
-              .then(displayUserResult)
-              .fail(displayError)
-              .done();
-              
-              break;
+              state = state.on().rgb(r, g, b).shortAlert();
+              console.log(HUE_LOG + ' Color set to ' + chalk.green('{' + parsedMessage.color.join(', ') + '}'));
+            } else {
+              return console.log('Invalid color format, needs to be {color: [r, g, b]}.');
             }
-          default:
-            {
-              return console.log('Cannot handle sub-topic ' + subTopic);
-            }
+          } catch (e) {
+            return console.log('Cannot parse message to valid JSON: ' + message);
+          }
+
+          break;
+        }
+        default: {
+          return console.log('Cannot handle sub-topic "' + subTopic + '"');
         }
       }
-    default:
-      {
+      break;
+  }
+    case 'config': {
+      if (topicPaths.length < 2 || !topics.config.topics[topicPaths[1]]) {
+        return console.log('Trying to handle /lights without valid sub-topic. ');
+      }
+      var subTopic = topicPaths[1];
+
+      switch (subTopic) {
+        case 'adduser': {
+          function displayUserResult(user) {
+            console.log('[HUE] Created new user:');
+            console.log('[HUE] ' + JSON.stringify(user, null, 2));
+
+            username = user.username
+          }
+
+          function displayError(error) {
+            console.log('[HUE] Error creating new user:');
+            console.log('[HUE] ' + error);
+          }
+
+          if (nodeUserExists()) {
+            return console.log('[HUE] User "Node API" already exists');
+          }
+
+          HueApi.registerUser(host, name)
+          .then(displayUserResult)
+          .fail(displayError)
+          .done();
+          
+          break;
+        }
+        default: {
+          return console.log('Cannot handle sub-topic ' + subTopic);
+        }
+      }
+    }
+    default: {
         return console.log('Cannot handle topic ' + topicPaths.join('/'));
-      }
+    }
   }
 }
 
@@ -172,4 +164,22 @@ function nodeUserExists() {
   }
 
   api.registeredUsers().then(displayResult).done();
+}
+
+// FIXME: Loop recursively for more flexibility
+function generateActiveTopics() {
+  var result = [];
+  
+  Object.keys(topics).forEach(function(key) {
+    var arr = [];
+    
+    Object.keys(topics[key].topics).forEach(function(subKey) {
+      arr.push([key, subKey].join('/'));
+    });
+    
+    result.push(arr);
+  });
+    
+  // Flatten the array to one dimension
+  return [].concat.apply([], result);
 }
